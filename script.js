71f9f0a5516a9191f860de5d58642addd3e4942d
Spratch <contact@josephclenet.fr>
2022-09-27 21:28:38 +0200

    simplification css

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-/**
-$( document ).ready( function() {
-  $('#services span').click(function(){
-  	value = $(this).data('filter');
-
-    $('#services span').removeClass('active'),
-  	$(this).addClass('active');
-
-  	$('#projets div.projet').each(function(){
-  		el = $(this);
-
-  		el.show();
-
-  		if(!el.hasClass(value) && value != "all" )
-  			el.hide();
-
-  	});
-  });
-});
-*/
 (function ($) {
 
   "use strict";
